ac1f7984b75c4e4a2b9b54e41854e823db6e51e7
mayaramdrs <mayara.m@escolar.ifrn.edu.br>
2021-12-31 14:01:36 -0300

    modulo cardapio

---
+++ cardapio.h
@@ -0,0 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                   ///
+///                   Universidade Federal do Rio Grande do Norte                     ///
+///                   Centro de Ensino Superior do Seridó                             ///
+///                   Departamento de Computação e Tecnologia                         ///
+///                   Disciplina DCT1106 - Programação                                ///
+///                   Projeto Sistema de Planejamento de Dieta                        ///
+///                   Desenvolvido por mariana                                        ///
+///                                                                                   ///
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//////
+////// Assisnaturas das funcões Módulo Cardápio
+//////
+
+void moduloCardapio(void);
+char telaMenuCardapio(void);
+void telaCadastrarCardapio(void);
+void telaPesquisarCardapio(void);
+void telaAlterarCardapio(void);
+void telaExcluirCardapio(void);
